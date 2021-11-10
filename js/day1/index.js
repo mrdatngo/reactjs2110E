91c6cfd99f78204e26d07393cdb224977a7b70e4
mrdatngo <mrdatngo@gmail.com>
2021-11-10 22:07:42 +0700

    js type

---
+++ js/day1/index.js
@@ -0,0 +1,32 @@
+var str = "Hello world"
+
+
+let isMale = false
+
+let huyStudent = {
+    name: "Huy",
+    age: 20
+}
+
+let huyName = huyStudent.name
+
+function hello() {
+    console.log("Hello")
+}
+
+hello()
+
+let test
+
+let firstNumber = 5
+// firstNumber+=5
+// firstNumber = firstNumber + 5
+// firstNumber++
+// ++firstNumber
+
+let secondNumber = 11
+let total = firstNumber++ + secondNumber
+
+console.log("total", total, isMale, huyStudent, huyName, test)
+
+// Number
