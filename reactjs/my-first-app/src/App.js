aa255b71f4a70a683b8f12e22b1866a1169f979e
mrdatngo <mrdatngo@gmail.com>
2022-01-03 20:56:46 +0700

    react-router-dome basic

--- reactjs/my-first-app/src/App.js
+++ reactjs/my-first-app/src/App.js
@@ -1,17 +1,45 @@
+import React, { useState } from 'react';
 import './App.css';
 import { MyClassComponent } from './study/func-class-component/MyClassComponent';
 import { MyFuncComponent } from './study/func-class-component/MyFuncComponent';
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <p>Func Component:</p>
-        <MyFuncComponent bgColor={"red"}/>
-      <hr />
-      <p>Class Component:</p>
-        <MyClassComponent bgColor={"blue"}/>
-    </div>
+    <Router>
+      <nav>
+        <Link to="/register-func">Function Component</Link>
+        <br />
+        <Link to="/register-class">Class Component</Link>
+        <br />
+        <a href="/register-func">Function Component</a>
+      </nav>
+      <Routes>
+        {/* <Route path="/register" element={"Test"}> */}
+        <Route path="/register-func" element={
+          <div>
+            <p>Func Component:</p>
+            <MyFuncComponent bgColor={"red"} />
+          </div>
+        } />
+        <Route path="/register-class"
+          element={
+            <div>
+              <p>Class Component:</p>
+              <MyClassComponent bgColor={"blue"} />
+            </div>
+          }
+        >
+        </Route>
+        {/* </Route> */}
+      </Routes>
+    </Router>
   );
 }
 
